d2fd4a5fa54927a32462b634c7dac5061e21bc07
ktkr5 <ktkr54321@gmail.com>
2024-02-28 13:59:07 +0800

    up calc1

---
+++ calc1/go.mod
@@ -0,0 +1,3 @@
+module calc1
+
+go 1.19
---
+++ calc1/main.go
@@ -0,0 +1,34 @@
+package main
+
+func add(a float64, b float64) (res float64) {
+	res = a + b
+	return
+}
+func minus(a float64, b float64) (res float64) {
+	res = a - b
+	return
+}
+func div(a float64, b float64) (res float64) {
+	res = a / b
+	return
+}
+func prod(a float64, b float64) (res float64) {
+	res = a * b
+	return
+}
+func calc(cmd string) (res float64) {
+	//1 parse the cmd
+	//2 pick the operation
+	//3 convert left and right number
+	//4 simplify cmd
+
+	//step
+	//10+6/3
+	//10+2
+	//12
+
+	return
+}
+func main() {
+
+}
